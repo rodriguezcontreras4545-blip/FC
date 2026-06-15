d35764fbd55989f359374f753ddc71717f34e1e1
JoAn <rodriguezcontreras4545@gmail.com>
2026-06-14 19:47:05 -0500

    edit 2

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const btnSoplar = document.getElementById("btn-soplar");
-    const seccionPastel = document.getElementById("seccion-pastel");
-    const seccionCarta = document.getElementById("seccion-carta");
+  const btnSoplar = document.getElementById("btn-soplar");
+  const seccionPastel = document.getElementById("seccion-pastel");
+  const seccionCarta = document.getElementById("seccion-carta");
 
-    btnSoplar.addEventListener("click", () => {
-        // Ocultamos el pastel
-        seccionPastel.classList.add("oculto");
-        
-        // Mostramos la carta quitando la clase 'oculto' y añadiendo la animación
-        seccionCarta.classList.remove("oculto");
-        seccionCarta.classList.add("mostrar-carta");
-    });
+  btnSoplar.addEventListener("click", () => {
+    // Ocultamos el pastel
+    seccionPastel.classList.add("oculto");
+
+    // Mostramos la carta quitando la clase 'oculto' y añadiendo la animación
+    seccionCarta.classList.remove("oculto");
+    seccionCarta.classList.add("mostrar-carta");
+  });
+  confetti({
+    particleCount: 150,
+    spread: 70,
+    origin: { y: 0.6 },
+  });
 });
